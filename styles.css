528541fd6264d4a644daf6bbb0c0d3bdf6164993
searcostumize <144778484+searcostumize@users.noreply.github.com>
2024-06-17 17:30:00 +0200

    Update styles.css

--- styles.css
+++ styles.css
@@ -1,1 +1,13 @@
+body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    margin: 0;
+    background-color: #282c34;
+}
+
+canvas {
+    border: 1px solid #fff;
+}
 
